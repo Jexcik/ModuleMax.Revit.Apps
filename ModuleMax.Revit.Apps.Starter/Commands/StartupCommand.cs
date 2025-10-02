c3f22ef37e3bd804eb747daf66fe13072e33784b
Jexcik <evstroyka@gmail.com>
2025-10-02 23:52:45 +0300

    WIP

--- ModuleMax.Revit.Apps.Starter/Commands/StartupCommand.cs
+++ ModuleMax.Revit.Apps.Starter/Commands/StartupCommand.cs
@@ -1,16 +1,15 @@
 ﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
 using Nice3point.Revit.Toolkit.External;
 
 namespace ModuleMax.Revit.Apps.Starter.Commands;
 
-/// <summary>
-///     External command entry point
-/// </summary>
 [UsedImplicitly]
 [Transaction(TransactionMode.Manual)]
 public class StartupCommand : ExternalCommand
 {
     public override void Execute()
     {
+        TaskDialog.Show("Revit","HelloWorld!");
     }
 }
