f3e08f76a19e033cddb08d55634d71f739fc4f69
Jexcik <evstroyka@gmail.com>
2026-02-27 01:02:21 +0300

    Work

--- install/Installer.cs
+++ install/Installer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using Installer;
 using WixSharp;
 using WixSharp.CommonTasks;
@@ -8,6 +10,8 @@ using Assembly = System.Reflection.Assembly;
 const string outputName = "ModuleMax.Revit.Apps";
 const string projectName = "ModuleMax.Revit.Apps";
 
+var iconsDirectory = ResolveIconsDirectory();
+
 var project = new Project
 {
     OutDir = "output",
@@ -16,13 +20,13 @@ var project = new Project
     UI = WUI.WixUI_FeatureTree,
     MajorUpgrade = MajorUpgrade.Default,
     GUID = new Guid("1358F4EF-295C-4BC2-92DF-142958908E46"),
-    BannerImage = @"install\Resources\Icons\BannerImage.png",
-    BackgroundImage = @"install\Resources\Icons\BackgroundImage.png",
+    BannerImage = Path.Combine(iconsDirectory, "BannerImage.png"),
+    BackgroundImage = Path.Combine(iconsDirectory, "BackgroundImage.png"),
     Version = Assembly.GetExecutingAssembly().GetName().Version.ClearRevision(),
     ControlPanelInfo =
     {
         Manufacturer = Environment.UserName,
-        ProductIcon = @"install\Resources\Icons\ShellIcon.ico"
+        ProductIcon = Path.Combine(iconsDirectory, "ShellIcon.ico")
     }
 };
 
@@ -31,6 +35,42 @@ project.RemoveDialogsBetween(NativeDialogs.WelcomeDlg, NativeDialogs.CustomizeDl
 
 BuildSingleUserMsi();
 BuildMultiUserUserMsi();
+
+string ResolveIconsDirectory()
+{
+    var roots = new[]
+    {
+        AppContext.BaseDirectory,
+        Environment.CurrentDirectory
+    }
+    .Select(Path.GetFullPath)
+    .Distinct(StringComparer.OrdinalIgnoreCase)
+    .ToArray();
+
+    foreach (var root in roots)
+    {
+        var directory = new DirectoryInfo(root);
+        while (directory is not null)
+        {
+            var installIcons = Path.Combine(directory.FullName, "install", "Resources", "Icons");
+            if (Directory.Exists(installIcons))
+            {
+                return installIcons;
+            }
+
+            var localIcons = Path.Combine(directory.FullName, "Resources", "Icons");
+            if (Directory.Exists(localIcons))
+            {
+                return localIcons;
+            }
+
+            directory = directory.Parent;
+        }
+    }
+
+    throw new DirectoryNotFoundException(
+        $"Cannot locate installer icons directory (install/Resources/Icons). BaseDirectory: '{AppContext.BaseDirectory}', CurrentDirectory: '{Environment.CurrentDirectory}'.");
+}
 
 void BuildSingleUserMsi()
 {
