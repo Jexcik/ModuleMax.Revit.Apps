c56490fb69d6f411a00c7132e23db9dc4693e092
Jexcik <evstroyka@gmail.com>
2026-03-17 22:52:41 +0300

    Work

--- build/Build.Publish.Clean.cs
+++ build/Build.Publish.Clean.cs
@@ -15,6 +15,6 @@ sealed partial class Build
         .Executes(() =>
         {
             Log.Information("Cleaning failed GitHub release");
-            GitTasks.Git($"push --delete origin {ReleaseVersion}", logInvocation: false);
+            GitTasks.Git($"push --delete origin refs/tags/{ReleaseVersion}", logInvocation: false);
         });
 }
